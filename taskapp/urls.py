1a5c6fc37b92929dfdd8873551a639532fb601a1
Ilundo <vanavakaruk4@email.com>
2025-04-19 06:33:54 +0300

    message

--- taskapp/urls.py
+++ taskapp/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
 
 from . import views
 
@@ -10,4 +12,4 @@ urlpatterns = [
   path("task/create/",views.TaskCreateView.as_view(),name="task-create"),
   path("task/edit/<int:pk>",views.TaskEditView.as_view(),name="task-edit"),
   path("task/delete/<int:pk>", views.TaskDeleteView.as_view(),name="task-delete"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
